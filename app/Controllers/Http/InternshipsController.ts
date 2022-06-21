6be15d1a0413582d223458d1257b9e484e995858
anarodrigues1996 <anadira.dira@gmail.com>
2022-06-21 20:40:11 -0100

    API Validation Add

--- app/Controllers/Http/InternshipsController.ts
+++ app/Controllers/Http/InternshipsController.ts
@@ -1,4 +1,5 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { schema } from '@ioc:Adonis/Core/Validator'
 
 import InternShip from "App/Models/InternShip"
 
@@ -11,13 +12,22 @@ export default class InternshipsController {
 
     public async add({request}: HttpContextContract){
 
+        const newPostSchema = schema.create({
+            name: schema.string(),
+            email: schema.string(),
+            phone: schema.number(),
+            birthday: schema.date()            
+        })
+        
+        const payload = await request.validate({ schema: newPostSchema })
+
         var intern = new InternShip()
         intern.name = request.input('name')
         intern.email = request.input('email')
         intern.phone = request.input('phone')
         intern.birthday = request.input('birthday')
         await intern.save()
-        
+
         return intern
     }
 
